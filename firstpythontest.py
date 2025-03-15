86cea902087006df4078474a5cc8f882d7d28b27
AgandeMajesty <agandemajesty@gmail.com>
2025-03-15 17:13:23 +0100

    Updated firstpythontest.py with latest changes

--- firstpythontest.py
+++ firstpythontest.py
@@ -10,7 +10,7 @@ driver.maximize_window()
 driver.find_element(By.ID, "user-name").send_keys("visual_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
-time.sleep(3)
+time.sleep(7)
 
 driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket").click()
 driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
@@ -19,23 +19,17 @@ driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
 driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
 driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
 driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-time.sleep(3)
+time.sleep(9)
 
 menu_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.ID, "react-burger-menu-btn"))
 )
 menu_button.click()
-time.sleep(2)  # Wait for the menu to open
-
-# Wait for the logout button to appear
+time.sleep(5)
 logout_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.ID, "logout_sidebar_link"))
 )
-
-# Click the logout button
 logout_button.click()
-
-# Wait and close the browser
 time.sleep(5)
 driver.quit()
 
